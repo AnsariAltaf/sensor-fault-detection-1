df47f68a7c4bbe42078c011ebd5be074d68f8c21
AnsariAltaf <ansarialtaf23.aa@gmail.com>
2022-11-04 20:44:45 +0530

    Data Ingestion done ,train_test_split pending

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.logger import logging
+from sensor.exception import SensorException
+import os,sys
+from sensor.pipeline.training_pipeline import TrainingPipeline
 
 if __name__ == '__main__':
 
-    mongodb_client=MongoDBClient()
-    print(mongodb_client.database.list_collection_names())
-    
+    training_pipeline = TrainingPipeline()
+    training_pipeline.run_pipeline()
